312a5a5b63311801d4688d22d4dd12851330b99e
Sergiojaquez07 <smje07@gmail.com>
2020-04-04 01:15:01 -0400

    base de datos

---
+++ SQLQuery4.sql
@@ -0,0 +1,12 @@
+create table Usuario
+(
+Id int primary kEY not null,
+Nombre varchar (50) not null,
+Correo varchar (60) not null,
+Contraseña varchar (60) not null,
+)
+use guardar
+select * from Usuario
+select * from Desapareci
+select * from imagenes
+
